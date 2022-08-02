ce6520fc8081c597417391d5c5d705671469b4dd
5ock <ccrubio5@gmail.com>
2022-08-02 10:45:10 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  
-  const iOSOpenApp = () => {
-    let before = new Date().valueOf()
-    setTimeout(function () {
-      let after = new Date().valueOf()
-      if(after - before > 2000)
-        return
-      window.location.href = 'https://apps.apple.com/tw/app/%E9%AB%98%E9%9B%84%E6%B8%AF%E4%BA%A4%E9%A0%98%E6%AB%83%E9%A0%90%E5%A0%B1%E7%B3%BB%E7%B5%B1/id1525326425'
-    }, 1000);
-  }
-  
+function App() {  
   return (
     <div className="App">
       <header className="App-header">

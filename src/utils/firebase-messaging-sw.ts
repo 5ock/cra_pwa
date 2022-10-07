9645c0780dd2bf6a231ef419541ec0d5218d3361
5ock <ccrubio5@gmail.com>
2022-10-07 16:58:29 +0800

    t

--- src/utils/firebase-messaging-sw.ts
+++ src/utils/firebase-messaging-sw.ts
@@ -19,4 +19,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebase);
-getToken(messaging, {vapidKey: "BD3uFRpJMZqUzjAvzsPl8a2K0FvN4Ab4irrNQzcCwZkTeyF9INHH38h9ys0p6FQbZKeShwQCs-38Mo_J0qxL-aA"});
+getToken(messaging, {vapidKey: "BD3uFRpJMZqUzjAvzsPl8a2K0FvN4Ab4irrNQzcCwZkTeyF9INHH38h9ys0p6FQbZKeShwQCs-38Mo_J0qxL-aA"}).then((currentToken) => {
+  if (currentToken) {
+    console.log(currentToken)
+    // Send the token to your server and update the UI if necessary
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
